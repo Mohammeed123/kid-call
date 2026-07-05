077f5e744a489253bab1aad996c35ef507d2ba0b
Mohammeed123 <mohameedalyounes3@gmail.com>
2026-07-06 02:44:55 +0300

    feat: append call log row to public.call_logs on callKid
    Closes #3

--- kids/kids.js
+++ kids/kids.js
@@ -95,7 +95,17 @@ export async function callKid(req, res, next) {
   if (callError) {
     throw new AppError("Could not create call", 400, callError);
   }
-
+  const { data: callLog, error: callLogError } = await client
+    .from("call_logs")
+    .insert({
+      user_id,
+      kid_id,
+    })
+    .select("*")
+    .single();
+  if (callLogError) {
+    throw new AppError("Could not create call log", 400, callLogError);
+  }
   const { data: response, error: responseError } = await client
     .from("kids")
     .select("*")
@@ -108,6 +118,4 @@ export async function callKid(req, res, next) {
       kid: response,
     },
   });
-
-  res.send({ message: `Calling kid with id ${kid_id}` });
 }
